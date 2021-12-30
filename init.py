74419fc48f2f2e440c86eecd0c3323449b07d0c2
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2021-12-30 16:58:52 -0500

    init: netrc

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 """
 init.py
     -- create folders for processing
+    -- check netrc for the downloading
 """
 
 import os, sys
@@ -13,6 +14,26 @@ config.read(os.path.join(BASE_DIR,"uavsar_processing.cfg"))
 
 items = config.items("general")
 for key, apath in items:
-    if not os.path.exists(apath):
-        print(f"creating: {key}")
-        os.makedirs(os.path.expanduser(apath), exist_ok=True)
+    apathfull = os.path.expanduser(apath)
+    if not os.path.exists(apathfull):
+        print(f"creating: {key} {apath}")
+        os.makedirs(apathfull, exist_ok=True)
+
+# https://urs.earthdata.nasa.gov/documentation/for_users/data_access/curl_and_wget
+homedir = os.path.expanduser("~")
+print(homedir)
+os.chdir(homedir)
+if not os.path.exists(".netrc"):
+    print("create .netrc")
+    cmd = "touch .netrc"
+    os.system(cmd)
+    cmd = 'echo "machine urs.earthdata.nasa.gov login quakesimuser password ???" > .netrc'
+    os.system(cmd)
+    cmd = "chmod 0600 .netrc"
+    os.system(cmd)
+    print("please fill in the password in .netrc")
+
+if not os.path.exists(".urs_cookies"):
+    print("create .usr_cookies")
+    cmd = "touch .urs_cookies"
+    os.system(cmd)
